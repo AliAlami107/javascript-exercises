5fa7c35b817234fa71b0b53f27e842f1b6dac1f2
AliAlami107 <aal107@uit.no>
2025-11-03 16:58:25 +0100

    solution to 07 reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,23 @@
-const reverseString = function() {
+/*  The goal is to reverse a string
+    1. get a string, start a loop from it's last index
+    2. loop down for each character and add it to a new string variable
+    3. then return the new string var
 
+
+*/
+
+const reverseString = function(string) {
+    //check for empty string
+    if(string === '') {
+        return '';
+    }
+
+    let new_string = "";
+
+    for(let i = string.length -1; i >= 0; i--) {
+        new_string += string[i];
+    }
+    return new_string;
 };
 
 // Do not edit below this line
