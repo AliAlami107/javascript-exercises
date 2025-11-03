9cdfc5e58fb2f09e54212f60218136d59369d10f
AliAlami107 <aal107@uit.no>
2025-11-03 16:36:22 +0100

    solution to 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,27 @@
-const repeatString = function() {
+/*
+The goal is to add string x(num) times
+using loops.
+    1. we have a string that we want to check and modify
+    2. if num is less than 2 we can just return the original string
+    3 . if num is greater than 2 we have to add string num times to the string
+    4. find the last index of the string and add string again until num times
 
+*/
+
+
+const repeatString = function(string, num) {
+    let temp = "";
+
+    if(num < 0) {
+        return "ERROR";
+    }
+    if(num < 1) {
+        return temp;
+    }
+    for (let i = 0; i < num; i++) {
+        temp += string;
+    }
+    return temp;
 };
 
 // Do not edit below this line
